28ae2c2638baae3c34776e1d416c3a9afbcf720e
Zin Min Htet <zetminhtin@gmail.com>
2022-05-08 22:51:17 +0630

    added access control allow origin

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const config = {
   COOKIE_SECRET_KEY_2: process.env.COOKIE_SECRET_2,
 };
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(
   cors({
     origin: "http://localhost:3000",
