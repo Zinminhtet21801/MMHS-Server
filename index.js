600d16b37a842a3f1557b1d8eb0130031e21ee90
LuciferX12Z <61372682+LuciferX12Z@users.noreply.github.com>
2022-05-10 12:49:06 +0630

    fixing logout 2

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const config = {
 };
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mmhs-client1.vercel.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
